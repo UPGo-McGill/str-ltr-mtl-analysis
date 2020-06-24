8b99381ddf43042aef98d885f2c45e8ad5cd6e59
bdbmax <maximebdeblois@gmail.com>
2020-06-24 13:44:52 -0400

    principal residence added but to modify

--- R/01_import/a_str.R
+++ R/01_import/a_str.R
@@ -75,10 +75,13 @@ CTs <-
 
 boroughs <-
   read_sf("data/shapefiles/montreal_boroughs_2019.shp") %>% 
+  filter(TYPE == "Arrondissement") %>% 
   select(borough = NOM) %>% 
-  # st_set_crs(2950) %>%  ??
   st_transform(32618)
 
+zones <- 
+  read_sf("data/shapefiles/AffectationPU.shp") %>% 
+  st_transform(32618)
 
 # Add population data from the census
 
@@ -132,6 +135,115 @@ GH <-
 
 
 
+
+### Add principal residence fields #############################################
+
+
+## Experimental principal residence function
+
+strr_principal_residence <- 
+  function(property, daily, FREH, GH, start_date, end_date, 
+           field_name = principal_residence, sensitivity = 0.1) {
+    
+    start_date <- as.Date(start_date, origin = "1970-01-01")
+    end_date <- as.Date(end_date, origin = "1970-01-01")
+    
+    sens_n <- 
+      round(sensitivity * as.integer((end_date - start_date + 1)))
+    
+    pr_table <- tibble(property_ID = property$property_ID,
+                       listing_type = property$listing_type,
+                       host_ID = property$host_ID,
+                       housing = property$housing)
+    
+    pr_ML <- 
+      daily %>% 
+      group_by(property_ID) %>% 
+      summarize(multi = if_else(
+        sum(multi * (date >= start_date)) + sum(multi * (date <= end_date)) >= sens_n, 
+        TRUE, FALSE))
+    
+    pr_n <-
+      daily %>%  
+      filter(status %in% c("R", "A"), date >= start_date, date <= end_date) %>% 
+      count(property_ID, status) %>% 
+      group_by(property_ID) %>% 
+      summarize(n_available = sum(n),
+                n_reserved = sum(n[status == "R"]))
+    
+    pr_table <- 
+      pr_table %>% 
+      left_join(pr_ML, by = "property_ID") %>% 
+      mutate(multi = if_else(is.na(multi), FALSE, multi)) %>% 
+      left_join(pr_n, by = "property_ID") %>% 
+      group_by(host_ID, listing_type) %>% 
+      mutate(LFRML = case_when(
+        listing_type != "Entire home/apt" ~ FALSE,
+        multi == FALSE                       ~ FALSE,
+        n_available == min(n_available)   ~ TRUE,
+        TRUE                              ~ FALSE)) %>% 
+      ungroup()
+    
+    pr_table <- 
+      pr_table %>%
+      filter(LFRML == TRUE) %>%
+      group_by(host_ID) %>%
+      mutate(prob = sample(0:10000, n(), replace = TRUE),
+             LFRML = if_else(
+               sum(LFRML) > 1 & prob != max(prob), FALSE, LFRML)) %>%
+      ungroup() %>% 
+      select(property_ID, LFRML2 = LFRML) %>% 
+      left_join(pr_table, ., by = "property_ID") %>% 
+      mutate(LFRML = if_else(!is.na(LFRML2), LFRML2, LFRML)) %>% 
+      select(-LFRML2)
+    
+    GH_list <-
+      GH %>% 
+      filter(date >= start_date, date <= end_date) %>% 
+      pull(property_IDs) %>%
+      unlist() %>%
+      tibble(property_ID = .) %>% 
+      group_by(property_ID) %>% 
+      filter(n() >= sens_n) %>% 
+      ungroup() %>% 
+      pull(property_ID) %>% 
+      unique()
+    
+    pr_table <-
+      pr_table %>% 
+      mutate(GH = if_else(property_ID %in% GH_list, TRUE, FALSE))
+    
+    pr_table <-
+      FREH %>% 
+      filter(date >= start_date, date <= end_date) %>% 
+      group_by(property_ID) %>% 
+      summarize(FREH = if_else(n() >= sens_n, TRUE, FALSE)) %>% 
+      left_join(pr_table, ., by = "property_ID") %>% 
+      mutate(FREH = if_else(is.na(FREH), FALSE, FREH))
+    
+    # Add principal_res field
+    
+    pr_table <- 
+      pr_table %>% 
+      mutate({{ field_name }} := case_when(
+        housing == FALSE               ~ FALSE,
+        GH == TRUE                     ~ FALSE,
+        listing_type == "Shared room"  ~ TRUE,
+        listing_type == "Private room" ~ TRUE,
+        FREH == TRUE                   ~ FALSE,
+        LFRML == TRUE                  ~ TRUE,
+        multi == TRUE                     ~ FALSE,
+        TRUE                           ~ TRUE)) %>% 
+      select(property_ID, {{ field_name }})
+    
+    left_join(property, pr_table, by = "property_ID")
+    
+  }
+
+property <- 
+  property %>% 
+  strr_principal_residence(daily, FREH, GH, key_date, key_date, 
+                           principal_res, 0.5)
 
 
 
