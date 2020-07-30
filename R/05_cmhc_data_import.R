f5ae32faa697e7811275b518afe8093f15185925
jxiao021 <joanna.xiao@mail.mcgill.ca>
2020-07-30 16:55:07 -0700

    yoy avg rent

--- R/05_cmhc_data_import.R
+++ R/05_cmhc_data_import.R
@@ -793,6 +793,412 @@ city_vac_rate$Year <- as.numeric(city_vac_rate$Year)
 save(city_vac_rate, file = "output/montreal_yoy_vac_rate.Rdata")
 
 
+#########average rents YOY file cleaning wooooooooo ############# 
+
+#####2019#####
+
+###import data 
+
+rent_2019 <- read_xlsx("data/average-rents-vacant-occupied-units-2019-en.xlsx")
+
+# Turn rows 1 and 2 into column names
+
+names(rent_2019) <- 
+  paste("column", 1:length(rent_2019), sep = "_")
+
+names(rent_2019)[[1]] <- 
+  "Zone"
+
+# Remove useless rows and columns
+rent_2019 <- 
+  rent_2019 %>% 
+  slice(-1, -2, -3, -4, -5, -6, -7) %>% 
+  select(-7, -12, -17, -22, -27)
+
+# rename again 
+names(rent_2019) <- 
+  paste("column", 1:length(rent_2019), sep = "_")
+
+## get what's needed from the file (totals)
+rent_2019 <-
+  rent_2019 %>% 
+  select(1, 2, 21, 22)
+
+
+##rename columns 
+names(rent_2019)[[1]] <- 
+  "Zone"
+
+names(rent_2019)[[2]] <- 
+  "Year"
+
+names(rent_2019)[[3]] <- 
+  "Occupied_Units"
+
+names(rent_2019)[[4]] <- 
+  "DQ"
+
+# Remove first two rows
+rent_2019 <- 
+  rent_2019 %>% 
+  slice(-1, -2)
+
+
+##filter for montreal rows 
+
+rent_2019 <-
+  rent_2019 %>% 
+  slice(110:149)
+
+### island is only 1:18 
+
+rent_2019 <-
+  rent_2019 %>% 
+  slice(1:18)
+
+# clean up zone column 
+
+rent_2019 <- separate(data = rent_2019, col = Zone, into = c("Zone_Number", "Zone_Name"), sep = " - ")
+
+rent_2019 <- 
+  separate(data = rent_2019, col = "Zone_Number", into = c("Zone", "Zone_Number"), sep = " ") 
+
+rent_2019 <- 
+  rent_2019 %>% 
+  select(-Zone)
+
+
+#####2018#####
+
+###import data 
+
+rent_2018 <- read_xlsx("data/average-rents-vacant-occupied-units-2018-en.xlsx")
+
+# Turn rows into column names
+
+names(rent_2018) <- 
+  paste("column", 1:length(rent_2018), sep = "_")
+
+# Remove useless rows and columns
+rent_2018 <- 
+  rent_2018 %>% 
+  slice(-1, -2, -3, -4, -5, -6, -7) %>% 
+  select(-7, -12, -17, -22, -27)
+
+# rename again 
+names(rent_2018) <- 
+  paste("column", 1:length(rent_2018), sep = "_")
+
+## get what's needed from the file (totals)
+rent_2018 <-
+  rent_2018 %>% 
+  select(1, 2, 21, 22)
+
+
+##rename columns 
+names(rent_2018)[[1]] <- 
+  "Zone"
+
+names(rent_2018)[[2]] <- 
+  "Year"
+
+names(rent_2018)[[3]] <- 
+  "Occupied_Units"
+
+names(rent_2018)[[4]] <- 
+  "DQ"
+
+# Remove first two rows
+rent_2018 <- 
+  rent_2018 %>% 
+  slice(-1, -2)
+
+
+##filter for montreal rows 
+
+rent_2018 <-
+  rent_2018 %>% 
+  slice(110:149)
+
+### island is only 1:18 
+
+rent_2018 <-
+  rent_2018 %>% 
+  slice(1:18)
+
+# clean up zone column 
+
+rent_2018 <- separate(data = rent_2018, col = Zone, into = c("Zone_Number", "Zone_Name"), sep = " - ")
+
+rent_2018 <- 
+  separate(data = rent_2018, col = "Zone_Number", into = c("Zone", "Zone_Number"), sep = " ") 
+
+rent_2018 <- 
+  rent_2018 %>% 
+  select(-Zone)
+
+#####2017#####
+
+###import data 
+
+rent_2017 <- read_xlsx("data/average-rents-vacant-occupied-units-2017-en.xlsx")
+
+# Turn rows into column names
+
+names(rent_2017) <- 
+  paste("column", 1:length(rent_2017), sep = "_")
+
+# Remove useless rows and columns
+rent_2017 <- 
+  rent_2017 %>% 
+  slice(-1, -2, -3, -4, -5, -6, -7) %>% 
+  select(-7, -12, -17, -22, -27)
+
+# rename again 
+names(rent_2017) <- 
+  paste("column", 1:length(rent_2017), sep = "_")
+
+## get what's needed from the file (totals)
+rent_2017 <-
+  rent_2017 %>% 
+  select(1, 2, 21, 22)
+
+
+##rename columns 
+names(rent_2017)[[1]] <- 
+  "Zone"
+
+names(rent_2017)[[2]] <- 
+  "Year"
+
+names(rent_2017)[[3]] <- 
+  "Occupied_Units"
+
+names(rent_2017)[[4]] <- 
+  "DQ"
+
+# Remove first two rows
+rent_2017 <- 
+  rent_2017 %>% 
+  slice(-1, -2)
+
+
+##filter for montreal rows 
+
+rent_2017 <-
+  rent_2017 %>% 
+  slice(110:149)
+
+### island is only 1:18 
+
+rent_2017 <-
+  rent_2017 %>% 
+  slice(1:18)
+
+# clean up zone column 
+
+rent_2017 <- separate(data = rent_2017, col = Zone, into = c("Zone_Number", "Zone_Name"), sep = " - ")
+
+rent_2017 <- 
+  separate(data = rent_2017, col = "Zone_Number", into = c("Zone", "Zone_Number"), sep = " ") 
+
+rent_2017 <- 
+  rent_2017 %>% 
+  select(-Zone)
+
+
+#####2016#####
+
+###import data 
+
+rent_2016 <- read_xlsx("data/average-rents-vacant-occupied-units-2016-en.xlsx")
+
+# Turn rows into column names
+
+names(rent_2016) <- 
+  paste("column", 1:length(rent_2016), sep = "_")
+
+# Remove useless rows and columns
+rent_2016 <- 
+  rent_2016 %>% 
+  slice(-1, -2, -3, -4, -5, -6, -7) %>% 
+  select(-7, -12, -17, -22, -27)
+
+# rename again 
+names(rent_2016) <- 
+  paste("column", 1:length(rent_2016), sep = "_")
+
+## get what's needed from the file (totals)
+rent_2016 <-
+  rent_2016 %>% 
+  select(1, 2, 21, 22)
+
+
+##rename columns 
+names(rent_2016)[[1]] <- 
+  "Zone"
+
+names(rent_2016)[[2]] <- 
+  "Year"
+
+names(rent_2016)[[3]] <- 
+  "Occupied_Units"
+
+names(rent_2016)[[4]] <- 
+  "DQ"
+
+# Remove first two rows
+rent_2016 <- 
+  rent_2016 %>% 
+  slice(-1, -2)
+
+
+##filter for montreal rows 
+
+rent_2016 <-
+  rent_2016 %>% 
+  slice(105:140)
+
+### island is only 1:18 
+
+rent_2016 <-
+  rent_2016 %>% 
+  slice(1:18)
+
+# clean up zone column 
+
+rent_2016 <- separate(data = rent_2016, col = Zone, into = c("Zone_Number", "Zone_Name"), sep = " - ")
+
+rent_2016 <- 
+  separate(data = rent_2016, col = "Zone_Number", into = c("Zone", "Zone_Number"), sep = " ") 
+
+rent_2016 <- 
+  rent_2016 %>% 
+  select(-Zone)
+
+
+#####2015#####
+
+###import data 
+
+rent_2015 <- read_xlsx("data/average-rents-vacant-occupied-units-2015-en.xlsx")
+
+# Turn rows into column names
+
+names(rent_2015) <- 
+  paste("column", 1:length(rent_2015), sep = "_")
+
+# Remove useless rows and columns
+rent_2015 <- 
+  rent_2015 %>% 
+  slice(-1, -2, -3, -4, -5, -6, -7) %>% 
+  select(-7, -12, -17, -22, -27)
+
+# rename again 
+names(rent_2015) <- 
+  paste("column", 1:length(rent_2015), sep = "_")
+
+## get what's needed from the file (totals)
+rent_2015 <-
+  rent_2015 %>% 
+  select(1, 2, 21, 22)
+
+
+##rename columns 
+names(rent_2015)[[1]] <- 
+  "Zone"
+
+names(rent_2015)[[2]] <- 
+  "Year"
+
+names(rent_2015)[[3]] <- 
+  "Occupied_Units"
+
+names(rent_2015)[[4]] <- 
+  "DQ"
+
+# Remove first two rows
+rent_2015 <- 
+  rent_2015 %>% 
+  slice(-1, -2)
+
+
+##filter for montreal rows 
+
+rent_2015 <-
+  rent_2015 %>% 
+  slice(105:140)
+
+### island is only 1:18 
+
+rent_2015 <-
+  rent_2015 %>% 
+  slice(1:18)
+
+# clean up zone column 
+
+rent_2015 <- separate(data = rent_2015, col = Zone, into = c("Zone_Number", "Zone_Name"), sep = " - ")
+
+rent_2015 <- 
+  separate(data = rent_2015, col = "Zone_Number", into = c("Zone", "Zone_Number"), sep = " ") 
+
+rent_2015 <- 
+  rent_2015 %>% 
+  select(-Zone)
+
+
+##### bind avg rent from 2015 - 2019 ####
+
+rent_total <- bind_rows(rent_2015, rent_2016, rent_2017, rent_2018, rent_2019)
+
+names(rent_total)[[4]] <- "Avg_Rent"
+
+
+####city of montreal avg rents #### 
+
+rent_city <- read_xls("data/VDM_AVG_RENT.xls")
+
+###remove excess info & rename
+
+rent_city <- 
+  rent_city %>% 
+  slice(-21:-28)
+
+rent_city <- 
+  rent_city %>% 
+  select(1, 10, 11)
+
+
+names(rent_city) <- 
+  paste("column", 1:length(rent_city), sep = "_")
+
+names(rent_city)[[1]] <- "Year"
+names(rent_city)[[2]] <- "Avg_Rent"
+names(rent_city)[[3]] <- "DQ"
+
+rent_city <- 
+  rent_city %>% 
+  slice(16:20)
+
+#clean up year column 
+
+rent_city <- separate(data = rent_city, col = Year, into = c("Year", "Month"), sep = " ")
+
+rent_city <- 
+  rent_city %>% 
+  select(-Month)
+
+
+rent_city <- 
+  rent_city %>% 
+  mutate(Zone_Name = "City of Montreal", Zone_Number = "N/A")
+
+#####BIND TO EXISTING FILE
+
+rent_total <- bind_rows(rent_total, rent_city)
+
+
+
 
 
 
