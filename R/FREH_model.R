13068c971e8044c5fc58e4f15203fcf058dab10a
David Wachsmuth <david.wachsmuth@mcgill.ca>
2020-07-31 16:51:53 -0400

    New FREH model for existing listings, to capture external shocks (regulation, pandemic, etc.)

--- R/FREH_model.R
+++ R/FREH_model.R
@@ -13,6 +13,7 @@ FREH <-
 
 year_long <- 
   daily %>% 
+  filter(date <= "2019-12-31") %>% 
   filter(listing_type == "Entire home/apt") %>% 
   select(property_ID, date, status) %>% 
   group_by(property_ID) %>% 
@@ -91,3 +92,47 @@ mean(predicted_classes == test_data$FREH)
 
 
 
+
+# Model based on last three months ----------------------------------------
+
+# Summarize by month
+after_one_year_month <- 
+  year_long %>% 
+  group_by(property_ID, year, month) %>% 
+  summarize(month_since_created = first(month_since_created),
+            month_R = sum(status == "R"),
+            month_AR = sum(status %in% c("R", "A")),
+            FREH = as.logical(ceiling(sum(FREH)))) %>% 
+  ungroup() %>%
+  group_by(property_ID) %>% 
+  mutate(month = month.name[.data$month],
+         cum_R = slide_int(month_R, sum, .before = 2, .complete = TRUE),
+         cum_AR = slide_int(month_AR, sum, .before = 2, .complete = TRUE)) %>% 
+  ungroup() %>% 
+  filter(!is.na(cum_R), !is.na(cum_AR), month_since_created >= 12)
+
+
+# Fit model ---------------------------------------------------------------
+
+model_after <- glm(FREH ~ cum_R + cum_AR + month, data = after_one_year_month, 
+             family = binomial)
+
+
+# Model testing -----------------------------------------------------------
+
+# Split the data into training and test set
+training_samples_after <- 
+  after_one_year_month$FREH %>% createDataPartition(p = 0.80, list = FALSE)
+
+train_data_after <- after_one_year_month[training_samples_after, ]
+test_data_after <- after_one_year_month[-training_samples_after, ]
+
+# Fit the model
+model_after <- glm(FREH ~ cum_R + cum_AR + month, data = train_data_after, 
+             family = binomial)
+
+# Test model
+probabilities_after <- 
+  model_after %>% predict(test_data_after, type = "response")
+predicted_classes_after <- ifelse(probabilities_after > 0.5, "TRUE", "FALSE")
+mean(predicted_classes_after == test_data_after$FREH)
