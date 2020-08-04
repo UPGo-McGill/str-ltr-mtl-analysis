59b9bb542baf146f53a0409eea69595ba2312cbf
David Wachsmuth <david.wachsmuth@mcgill.ca>
2020-08-03 21:20:11 -0400

    FREH model script (11) now finished

--- R/11_FREH_model.R
+++ R/11_FREH_model.R
@@ -1,4 +1,17 @@
-#### New FREH modelling attempt ################################################
+#### 11 FREH MODEL #############################################################
+
+#' This script is moderately time-consuming to run; it should be rerun whenever 
+#' STR data changes.
+#' 
+#' Output:
+#' - `str_processed.Rdata` (updated)
+#' - `FREH_model.Rdata`
+#' 
+#' Script dependencies:
+#' - `09_str_processing.R`
+#' 
+#' External dependencies:
+#' - None
 
 source("R/01_startup.R")
 library(caret)
@@ -10,25 +23,31 @@ library(slider)
 load("output/str_processed.Rdata")
 
 
-# Wrangle data ------------------------------------------------------------
+# Prepare daily file ------------------------------------------------------
+
+daily <- 
+  daily %>% 
+  mutate(year = as.numeric(substr(date, 1, 4)),
+         month = as.numeric(substr(date, 6, 7)))
+
+
+# Get traditional FREH status ---------------------------------------------
 
 FREH <- 
-  FREH %>% 
-  mutate(FREH = TRUE)
-
-year_long <- 
   daily %>% 
-  filter(date <= "2019-12-31") %>% 
+  strr_FREH() %>% 
+  filter(FREH)
+
+
+# Produce monthly activity table for all EH listings ----------------------
+
+monthly <-
+  daily %>% 
   filter(listing_type == "Entire home/apt") %>% 
-  select(property_ID, date, status) %>% 
-  group_by(property_ID) %>% 
-  filter(n() >= 395) %>% 
-  ungroup() %>% 
-  left_join(FREH) %>% 
-  mutate(FREH = if_else(is.na(FREH), FALSE, FREH),
-         year = as.numeric(substr(date, 1, 4)),
-         month = as.numeric(substr(date, 6, 7))) %>% 
-  left_join(select(property, property_ID, created)) %>% 
+  left_join(FREH, by = c("property_ID", "date")) %>% 
+  mutate(FREH = if_else(is.na(FREH), FALSE, FREH)) %>% 
+  left_join(select(st_drop_geometry(property), property_ID, created),
+            by = "property_ID") %>% 
   # Trim listings to the start of the month--can later test if the extra
   # complexity produces meaningful model improvement
   mutate(created = if_else(substr(created, 9, 10) == "01", created,
@@ -37,103 +56,170 @@ year_long <-
   mutate(created_year = as.numeric(substr(created, 1, 4)),
          created_month = as.numeric(substr(created, 6, 7)),
          month_since_created = (year - created_year) * 12 + 
-           (month - created_month))
-
-# Just take exactly one year of data
-one_year <- 
-  year_long %>% 
-  filter(date < created %m+% months(12))
-  
-# Summarize data by month
-year_month <- 
-  one_year %>% 
+           (month - created_month)) %>% 
   group_by(property_ID, year, month) %>% 
   summarize(month_since_created = first(month_since_created),
-            month_R = sum(status == "R"),
-            month_AR = sum(status %in% c("R", "A"))) %>% 
+            R = sum(status == "R"),
+            A = sum(status == "A"),
+            B = sum(status == "B"),
+            FREH = as.logical(ceiling(mean(FREH))),
+            .groups = "drop")
+
+
+# Get listings active for a year or more, and take the first year ---------
+
+first_year <-
+  monthly %>% 
+  filter(year <= 2019) %>% 
+  group_by(property_ID) %>% 
+  filter(n() >= 12) %>% 
+  ungroup() %>% 
+  filter(month_since_created <= 11) %>% 
+  group_by(property_ID) %>% 
+  mutate(FREH = as.logical(ceiling(mean(FREH))),
+         month = month.name[.data$month],
+         cum_R = cumsum(R),
+         cum_A = cumsum(A),
+         cum_AR = cum_A + cum_R) %>% 
+  ungroup() %>% 
+  select(-cum_A)
+  
+
+# Fit model and apply to listings < 1 year old ----------------------------
+
+model_12 <- glm(FREH ~ cum_R + cum_AR + month_since_created + month, 
+             data = first_year, family = binomial)
+
+model_12_results <- 
+  monthly %>% 
+  group_by(property_ID) %>% 
+  filter(max(month_since_created) < 12) %>% 
+  ungroup() %>% 
+  group_by(property_ID) %>% 
+  mutate(FREH = as.logical(ceiling(mean(FREH))),
+         month = month.name[.data$month],
+         cum_R = cumsum(R),
+         cum_A = cumsum(A),
+         cum_AR = cum_A + cum_R) %>% 
+  ungroup() %>% 
+  select(-cum_A) %>% 
+  modelr::add_predictions(model_12, type = "response") %>% 
+  mutate(FREH = if_else(FREH, as.numeric(FREH), pred)) %>% 
+  select(-pred) %>% 
+  rowwise() %>% 
+  mutate(month = which(month.name == month)) %>% 
   ungroup()
 
-ever_FREH <- 
-  one_year %>% 
-  group_by(property_ID) %>% 
-  summarize(FREH = as.logical(ceiling(sum(FREH))))
-
-year_month <- 
-  year_month %>% 
-  left_join(ever_FREH) %>% 
-  group_by(property_ID) %>% 
-  mutate(month = month.name[.data$month],
-         cum_R = cumsum(month_R),
-         cum_AR = cumsum(month_AR))
+daily <-
+  daily %>% 
+  left_join(select(monthly, property_ID, year, month, FREH),
+            by = c("property_ID", "year", "month")) %>% 
+  left_join(select(model_12_results, property_ID:month, prob = FREH),
+            by = c("property_ID", "year", "month")) %>% 
+  mutate(FREH = case_when(
+    !is.na(prob) ~ prob,
+    !is.na(FREH) ~ as.numeric(FREH),
+    TRUE ~ 0)) %>% 
+  select(-prob)
   
 
-# Fit model ---------------------------------------------------------------
-
-model <- glm(FREH ~ cum_R + cum_AR + month_since_created + month, 
-             data = year_month, family = binomial)
-
-
-# Model testing -----------------------------------------------------------
-
-# Split the data into training and test set
-training_samples <- 
-  year_month$FREH %>% createDataPartition(p = 0.80, list = FALSE)
-
-train_data  <- year_month[training_samples, ]
-test_data <- year_month[-training_samples, ]
-
-# Fit the model
-model <- glm(FREH ~ cum_R + cum_AR + month_since_created + month, 
-             data = train_data, family = binomial)
-
-# Test model
-probabilities <- model %>% predict(test_data, type = "response")
-predicted_classes <- ifelse(probabilities > 0.5, "TRUE", "FALSE")
-mean(predicted_classes == test_data$FREH)
+# # Model testing -----------------------------------------------------------
+# 
+# # Split the data into training and test set
+# training_samples_12 <-
+#   first_year$FREH %>% 
+#   createDataPartition(p = 0.80, list = FALSE)
+# 
+# train_data_12 <- first_year[training_samples_12, ]
+# test_data_12 <- first_year[-training_samples_12, ]
+# 
+# # Fit the model
+# model_12_test <- glm(FREH ~ cum_R + cum_AR + month_since_created + month,
+#                   data = train_data_12, family = binomial)
+# 
+# # Test model
+# probabilities_12 <- model_12_test %>% predict(test_data_12, type = "response")
+# predicted_classes_12 <- ifelse(probabilities_12 > 0.5, "TRUE", "FALSE")
+# mean(predicted_classes_12 == test_data_12$FREH) # Outcome: 0.867
 
 
-
-
-# Model based on last three months ----------------------------------------
+# Model based on last n months --------------------------------------------
 
 # Summarize by month
-after_one_year_month <- 
-  year_long %>% 
-  group_by(property_ID, year, month) %>% 
-  summarize(month_since_created = first(month_since_created),
-            month_R = sum(status == "R"),
-            month_AR = sum(status %in% c("R", "A")),
-            FREH = as.logical(ceiling(sum(FREH)))) %>% 
-  ungroup() %>%
-  group_by(property_ID) %>% 
+after_one_year <- 
+  monthly %>%
   mutate(month = month.name[.data$month],
-         cum_R = slide_int(month_R, sum, .before = 2, .complete = TRUE),
-         cum_AR = slide_int(month_AR, sum, .before = 2, .complete = TRUE)) %>% 
+         AR = A + R) %>% 
+  group_by(property_ID) %>% 
+  mutate(R_3 = slide_int(R, sum, .before = 2, .complete = TRUE),
+         AR_3 = slide_int(AR, sum, .before = 2, .complete = TRUE)) %>% 
   ungroup() %>% 
-  filter(!is.na(cum_R), !is.na(cum_AR), month_since_created >= 12)
+  filter(!is.na(R_3), !is.na(AR_3), month_since_created >= 12)
 
 
-# Fit model ---------------------------------------------------------------
+# Fit models and apply to listings > 2 months -----------------------------
 
-model_after <- glm(FREH ~ cum_R + cum_AR + month, data = after_one_year_month, 
-             family = binomial)
+model_1 <- glm(FREH ~ R + AR + month, data = after_one_year, family = binomial)
+
+model_3 <- glm(FREH ~ R_3 + AR_3 + month, data = after_one_year, 
+               family = binomial)
+
+model_1_3_results <- 
+  monthly %>% 
+  mutate(month = month.name[.data$month],
+         AR = A + R) %>% 
+  group_by(property_ID) %>% 
+  mutate(R_3 = slide_int(R, sum, .before = 2, .complete = TRUE),
+         AR_3 = slide_int(AR, sum, .before = 2, .complete = TRUE)) %>% 
+  ungroup() %>% 
+  filter(!is.na(R_3), !is.na(AR_3)) %>% 
+  modelr::add_predictions(model_1, type = "response") %>% 
+  mutate(FREH_1 = pred) %>% 
+  select(-pred) %>% 
+  modelr::add_predictions(model_3, type = "response") %>% 
+  mutate(FREH_3 = pred) %>% 
+  select(-pred) %>% 
+  rowwise() %>% 
+  mutate(month = which(month.name == month)) %>% 
+  ungroup()
+
+daily <-
+  daily %>% 
+  left_join(select(model_1_3_results, property_ID, year, month, FREH_1, FREH_3),
+            by = c("property_ID", "year", "month")) %>% 
+  mutate(FREH_1 = if_else(is.na(FREH_1), 0, FREH_1),
+         FREH_3 = if_else(is.na(FREH_3), 0, FREH_3)) %>% 
+  select(-year, -month)
 
 
-# Model testing -----------------------------------------------------------
+# # Model testing -----------------------------------------------------------
+# 
+# # Split the data into training and test set
+# training_samples_1_3 <-
+#   after_one_year$FREH %>% createDataPartition(p = 0.80, list = FALSE)
+# 
+# train_data_1_3 <- after_one_year[training_samples_1_3, ]
+# test_data_1_3 <- after_one_year[-training_samples_1_3, ]
+# 
+# # Fit the models
+# model_1_test <- glm(FREH ~ R + AR + month, data = train_data_1_3,
+#                           family = binomial)
+# 
+# model_3_test <- glm(FREH ~ R_3 + AR_3 + month, data = train_data_1_3,
+#                           family = binomial)
+# 
+# # Test models
+# probabilities_1 <- model_1_test %>% predict(test_data_1_3, type = "response")
+# predicted_classes_1 <- ifelse(probabilities_1 > 0.5, "TRUE", "FALSE")
+# mean(predicted_classes_1 == test_data_1_3$FREH) # Outcome: 0.808
+# 
+# probabilities_3 <- model_3_test %>% predict(test_data_1_3, type = "response")
+# predicted_classes_3 <- ifelse(probabilities_3 > 0.5, "TRUE", "FALSE")
+# mean(predicted_classes_3 == test_data_1_3$FREH) # Outcome: 0.850
 
-# Split the data into training and test set
-training_samples_after <- 
-  after_one_year_month$FREH %>% createDataPartition(p = 0.80, list = FALSE)
 
-train_data_after <- after_one_year_month[training_samples_after, ]
-test_data_after <- after_one_year_month[-training_samples_after, ]
+# Save output -------------------------------------------------------------
 
-# Fit the model
-model_after <- glm(FREH ~ cum_R + cum_AR + month, data = train_data_after, 
-             family = binomial)
-
-# Test model
-probabilities_after <- 
-  model_after %>% predict(test_data_after, type = "response")
-predicted_classes_after <- ifelse(probabilities_after > 0.5, "TRUE", "FALSE")
-mean(predicted_classes_after == test_data_after$FREH)
+save(property, daily, host, GH, file = "output/str_processed.Rdata")
+save(FREH, monthly, first_year, model_12, model_12_results, after_one_year,
+     model_1, model_3, model_1_3_results, file = "output/FREH_model.Rdata")
