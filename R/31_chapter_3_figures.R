8995eb9bde5191aa07054d58eaa7ec75350153da
Cloe St-Hilaire <cloe.st-hilaire@mail.mcgill.ca>
2020-08-03 23:07:02 -0400

    Modifications on figure 3.1

--- R/31_chapter_3_figures.R
+++ R/31_chapter_3_figures.R
@@ -6,28 +6,24 @@ library(ggplot2)
 library(patchwork)
 library(data.table)
 
-### Colour palette #############################################################
 
-col_palette <- 
-  c("#FF3333", "#FFCC00", "#FF6600", "#66FFFF", "#00CC66")
+### FIGURE 3.1 - Housing units converted to dedicated STRs in Montreal ###########################################
 
-scales::show_col(col_palette)
-
-### FIGURE 8 - Housing units converted to dedicated STRs in Montreal ###########################################
-
-housing_graph <-
-  housing_loss %>% 
+housing_graph <- housing_loss %>% 
   mutate(`Listing type` = factor(
     `Listing type`, 
-    levels = c("Summer listings", "Private room", "Entire home/apt"))) %>% 
+    levels = c("Private room", "Entire home/apt"))) %>% 
   ggplot(aes(`Listing type`)) +
-  geom_col(aes(date, `Housing units`, fill = `Listing type`),
-           lwd = 0) +
+  geom_col(aes(date, `Housing units`, 
+               fill = `Listing type`), lwd = 0) +
   theme_minimal() +
-  #scale_y_continuous(name = NULL, label = scales::comma, limits = c(0, 200)) +
-  #scale_x_date(name = NULL, limits = c(as.Date("2016-10-01"), NA)) +
-  scale_fill_manual(values = col_palette[3:1]) +
+  scale_fill_manual(values = col_palette[c(1, 3, 2)]) +
+  scale_x_date(name = NULL, limits = c(as.Date("2016-10-01"), NA)) +
+  scale_y_continuous(name = NULL, label = scales::comma) +
+  annotate("rect", xmin = as.Date("2020-03-14"), xmax = as.Date("2020-06-25"), ymin = 0, ymax = 5200, alpha = .2)+
   theme(legend.position = "bottom", 
+        panel.grid.minor.x = element_blank(),
+        panel.grid.major.x = element_blank(),
         text = element_text(face = "plain"), #family = "Futura", 
         legend.title = element_text(face = "bold", #family = "Futura", 
                                     size = 10),
