0828c04c5836b292e4238b882b1cd28e7d77a345
Cloe St-Hilaire <cloe.st-hilaire@mail.mcgill.ca>
2020-08-18 10:00:51 -0400

    Added numbering to elements

--- R/40_chapter_4_analysis.R
+++ R/40_chapter_4_analysis.R
@@ -19,7 +19,7 @@ load("output/str_processed.Rdata")
 
 # Look at year-over-year growth of Reserved and Available listings over certain time periods ------------------------------------------------------
 
-#YOY growth of R number
+#' [1] Year-over-year growth of Reserved listings
 (daily %>% 
    filter(status == "R", date >= LTM_start_date, date <= LTM_end_date) %>% 
    nrow() -
@@ -31,7 +31,7 @@ load("output/str_processed.Rdata")
   filter(status == "R", date >= LTM_start_date - years(1), date <= LTM_end_date - years(1)) %>% 
   nrow()
 
-# growth of R for first half of 2020
+#' [2] growth of Reserved listings for the first half of 2020
 (daily %>% 
     filter(status == "R", date >= "2020-01-01", date <= max(date)) %>% 
     nrow() -
@@ -43,7 +43,7 @@ load("output/str_processed.Rdata")
   filter(status == "R", date >= "2019-01-01", date <= max(date) - years(1)) %>% 
   nrow()
 
-# Lowest point of As
+#' [3] Lowest point of available listings
 daily %>% 
   filter(status == "A", date >= "2018-01-01") %>% 
   count(date) %>% 
