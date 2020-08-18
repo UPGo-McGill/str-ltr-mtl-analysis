dec9938b760b78a0c38a00a95fd1c58986cac37f
Cloe St-Hilaire <cloe.st-hilaire@mail.mcgill.ca>
2020-08-18 09:43:30 -0400

    Separated the non-graph producing code from 41_figures

---
+++ R/40_chapter_4_analysis.R
@@ -0,0 +1,50 @@
+#### 40 CHAPTER 4 ANALYSIS ######################################################
+
+#' This script produces the facts for chapter 4. It runs quickly.
+#' 
+#' Output:
+#' - None
+#' 
+#' Script dependencies:
+#' - `09_str_processing.R`
+#' 
+#' External dependencies:
+#' - None
+
+source("R/01_startup.R")
+
+# Load previous data ------------------------------------------------------
+
+load("output/str_processed.Rdata")
+
+# Look at year-over-year growth of Reserved and Available listings over certain time periods ------------------------------------------------------
+
+#YOY growth of R number
+(daily %>% 
+   filter(status == "R", date >= LTM_start_date, date <= LTM_end_date) %>% 
+   nrow() -
+   daily %>% 
+   filter(status == "R", date >= LTM_start_date - years(1), date <= LTM_end_date - years(1)) %>% 
+   nrow()
+) /
+  daily %>% 
+  filter(status == "R", date >= LTM_start_date - years(1), date <= LTM_end_date - years(1)) %>% 
+  nrow()
+
+# growth of R for first half of 2020
+(daily %>% 
+    filter(status == "R", date >= "2020-01-01", date <= max(date)) %>% 
+    nrow() -
+    daily %>% 
+    filter(status == "R", date >= "2019-01-01", date <= max(date) - years(1)) %>% 
+    nrow()
+) /
+  daily %>% 
+  filter(status == "R", date >= "2019-01-01", date <= max(date) - years(1)) %>% 
+  nrow()
+
+# Lowest point of As
+daily %>% 
+  filter(status == "A", date >= "2018-01-01") %>% 
+  count(date) %>% 
+  arrange(n)
