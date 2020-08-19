3bc7532f3b5fdd4b800df386513fdfe44f1d64d0
Cloe St-Hilaire <cloe.st-hilaire@mail.mcgill.ca>
2020-08-19 11:34:24 -0400

    Added two other numbers

--- R/40_chapter_4_analysis.R
+++ R/40_chapter_4_analysis.R
@@ -57,9 +57,7 @@ daily_comp_ban <-
   strr_compress(filter(daily, date >= "2020-03-28", date <= "2020-06-25"))
 
 daily_comp_ban %>%
-  filter(booked_date >= "2020-03-28", booked_date <= "2020-06-25") %>% 
-  mutate(duration=end_date-start_date+1) %>% 
-  filter(duration >= 28)
+  filter(booked_date >= "2020-03-28", booked_date <= "2020-06-25")
 
 #' [2] Reservations between 28 march and 25 june 2019
 daily_comp_2019 <-
@@ -67,4 +65,25 @@ daily_comp_2019 <-
 
 daily_comp_2019 %>%
   filter(booked_date >= "2019-03-28", booked_date <= "2019-06-25")
+
+#' [3] Difference between the two years
+(
+  (daily_comp_ban %>%
+  filter(booked_date >= "2020-03-28", booked_date <= "2020-06-25") %>% 
+  nrow())
+  -
+  (daily_comp_2019 %>%
+  filter(booked_date >= "2019-03-28", booked_date <= "2019-06-25") %>% 
+  nrow())
+  )/(daily_comp_2019 %>%
+     filter(booked_date >= "2019-03-28", booked_date <= "2019-06-25") %>% 
+     nrow()
+   )
+
+#' [4] Reservations of more than 30 days during the ban
+daily_comp_ban %>%
+  filter(booked_date >= "2020-03-28", booked_date <= "2020-06-25") %>% 
+  mutate(duration=end_date-start_date+1) %>% 
+  filter(duration >= 30)
+
 
