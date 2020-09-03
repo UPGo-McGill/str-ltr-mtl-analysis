2dd191ab8d65f2f3431b38fa6f35d2cb9d9ac873
bdbmax <maximebdeblois@gmail.com>
2020-09-03 23:21:05 +0200

    Updated done until line 515

--- R/50_chapter_5_analysis.R
+++ R/50_chapter_5_analysis.R
@@ -47,12 +47,12 @@ ltr_unique_property_ID <-
 
 # How many STR listings have returned to the long-term market? ------------
 
-#' Our image matching algorithm recognized 2,569 [1] unique Airbnb listings 
+#' Our image matching algorithm recognized 2,526 [1] unique Airbnb listings 
 #' which matched with 4,842 [2] different LTR listings (as some units are posted 
 #' multiple times) in the City of Montreal. The matching LTR listings were 
 #' evenly split between Kijiji (2,596 [2] listings, or 53.6%) and Craigslist 
-#' (2,246 [2] listings, or 46.4%). Out of the 2,569 matching Airbnb listings,
-#' 57.6% (1,479 [3] listings) were active STRs in 2020, which establishes a 
+#' (2,246 [2] listings, or 46.4%). Out of the 2,526 matching Airbnb listings,
+#' 58.4% (1,475 [3] listings) were active STRs in 2020, which establishes a 
 #' lower bound for the number of unique housing units that went from the STR 
 #' market to the LTR market due to the COVID-19 pandemic.
 
@@ -94,7 +94,7 @@ property %>%
   nrow()
 
 
-#' Out of the 2,569 unique Airbnb listings which matched, 1,690 (66.9%) [1] were observed 
+#' Out of the 2,526 unique Airbnb listings which matched, 1,690 (66.9%) [1] were observed 
 #' on Kijiji. Of these listings, 74.0% [2] were identified by their hosts as 
 #' “long-term rentals” and 26.0% [2] were identified as “short-term rentals”. 
 #' Among these listings, 50.3% [3] specified lease lengths of 1 year, 21.9% [3] 
@@ -250,10 +250,17 @@ ltr_unique %>%
   mutate(matched = if_else(!is.na(property_ID), TRUE, FALSE)) %>% 
   group_by(matched) %>%
   summarize(avg_price = mean(price)) %>% 
-  summarize(pct_diff = (avg_price[2] - avg_price[1])/avg_price[2])
+  summarize(pct_diff = (avg_price[2] - avg_price[1]) / avg_price[2])
 
 
 # Analysis of the STRs that turned to the LTR platforms during the pandemic ---------------------------------------------------
+
+#' As we have seen in a previous section, frequently rented entire-home (FREH) 
+#' listings are likely to be operated commercially. They may be well-established 
+#' due to frequent reservations and consequently have more reviews. Of the 2,569 
+#' unique STR listings that matched with the LTR market, 50.4% [1] had at least three 
+#' months of reservations and availability which are consistent with a year-long 
+#' full-time operation.
 
 #' [1] Type of STR - FREH 
 ltr_unique_property_ID %>% 
@@ -262,23 +269,47 @@ ltr_unique_property_ID %>%
   ltr_unique_property_ID %>% 
   nrow()
 
-#' [2] Type of STR - GH 
-# ltr_unique_property_ID %>% 
-#   filter(property_ID %in% (GH %>% 
-#                       st_drop_geometry() %>% 
-#                       group_by(ghost_ID) %>% 
-#                       unnest(property_IDs) %>% 
-#                       pull(property_IDs))) %>% 
-#   nrow()
+#' As mentioned above, one indication that a listing might be operated for commercial 
+#' purposes is when a host operates more than one listing on a given date, as it is 
+#' unlikely that both listings could be operated out of their principal residence. 
+#' Out of all the STR listings that matched LTR listings, 1,903 (75.3%) [1] were found as 
+#' multilisting at least once in their lifetime.
 
-#' [3] Type of STR - Multilistings 
+#' [1] Type of STR - Multilistings 
+ltr_unique_property_ID %>% 
+  filter(property_ID %in% filter(daily, multi == T)$property_ID) %>% 
+  nrow()
+
 ltr_unique_property_ID %>% 
   filter(property_ID %in% filter(daily, multi == T)$property_ID) %>% 
   nrow() /
   ltr_unique_property_ID %>% 
   nrow()
 
-#' [4] Combination of FREH, GH and ML to get the listings with commercial characteristics
+#' If we aggregate the characteristics that can be used to determine whether a STR is 
+#' operated commercially (multilisting, FREH), there were 2,127 [1] matches that can be 
+#' categorized as being commercial operations, making up 84.2% [1] of all unique STR units 
+#' that matched to a LTR listing and were active in 2020. (Ghost hostels are not 
+#' included in this analysis since only postings for entire units on LTR platforms 
+#' were scraped.) However, only 8.4% of all STR listings in Montreal that could be 
+#' considered commercial operations in their lifetime were matched on longer-term 
+#' rental platforms during the pandemic. While commercial listings make up a 
+#' great majority of the matches, the low preponderance of commercial listings 
+#' resorting to listing their units on LTR platforms could mean that many commercial 
+#' operators decided to remain on STR platforms instead of finding longer-term tenants. 
+#' The commercial listings that have a good standing with numerous previous reservations 
+#' and reviews might have chosen to remain in the STR market, while mostly newer 
+#' commercial listings are popping up on Kijiji and Craigslist.
+
+#' [1] Combination of FREH, GH and ML to get the listings with commercial characteristics
+rbind(
+  ltr_unique_property_ID %>% 
+    filter(property_ID %in% filter(daily, multi == T)$property_ID),
+  ltr_unique_property_ID %>% 
+    filter(property_ID %in% filter(daily, FREH_3 > 0.5)$property_ID)) %>% 
+  distinct(property_ID) %>% 
+  nrow()
+
 (rbind(
   ltr_unique_property_ID %>% 
     filter(property_ID %in% filter(daily, multi == T)$property_ID),
@@ -289,20 +320,30 @@ ltr_unique_property_ID %>%
   ltr_unique_property_ID %>% 
   nrow()
 
-daily %>% 
-  filter(date >= "2020-01-01", 
-         property_ID %in% filter(daily, FREH_3 > 0.5 | multi == T)$property_ID, 
-         property_ID %in% ltr_unique_property_ID$property_ID) %>% 
-  count(property_ID) %>% 
-  nrow() / 
+ltr_unique_property_ID %>% 
+  nrow() /
   daily %>% 
-  filter(date >= "2020-01-01",
-         property_ID %in% filter(daily, FREH_3 > 0.5 | multi == T)$property_ID) %>% 
-  count(property_ID) %>% 
+  filter(status != "B", date == "2020-03-01", FREH_3 > 0.5 | multi == T) %>% 
   nrow()
 
+#' Type of host ---------------------------------------------------
 
-# Analysis of the type of host that turned to LTR platforms ---------------------------------------------------
+#' In Montreal, 1149 [1] unique STR host IDs were linked to the 2,526 LTR matches. 
+#' 288 [2] of these hosts posted more than one of their STR units on the LTR platforms. 
+#' For example, the top (mentioned in section 2) host posted 239 [2] of its STR units on 
+#' the LTR market. Half [3] of the active properties of these 1149 hosts were found on either 
+#' Kijiji, Craigslist or both. The fact that only a portion of the hosts’ listings were 
+#' found on LTR platforms indicates that these matches are likely to be an underestimation
+#'  of the STRs that were posted on LTR platforms. Indeed, it would be intuitive to 
+#'  assume that a host that decides to post its listings on a LTR platform would post 
+#'  all of them. This could mean that the remainder of the listings were posted and 
+#'  rented before the team conducted the weekly scrapes, that only the hosts’ less 
+#'  performing STRs were put onto the LTR rental market, hosts limited themselves to 
+#'  other LTR listings websites (such as various Facebook housing groups), sold housing 
+#'  formerly used as STRs or simply that the hosts used updated pictures for its 
+#'  listings, making it impossible to detect these matches through our image matching 
+#'  algorithm. This minority of each host’s listings found is thus an example of how 
+#'  the matches found are likely substantially lower than the reality.
 
 #' [1] number of hosts found on a LTR platform
 property %>%
@@ -313,25 +354,20 @@ property %>%
 #' [2] number of hosts having more than one properties on LTR market
 property %>%
   st_drop_geometry() %>% 
-  filter(!is.na(ltr_ID), scraped >= "2020-01-01") %>% 
+  filter(!is.na(ltr_ID)) %>% 
   count(host_ID) %>% 
   filter(n > 1) %>% 
-  arrange(desc(n))
-
-property %>% 
-  st_drop_geometry() %>% 
-  filter(scraped >= "2020-01-01", host_ID %in% (property %>%
-                                                  st_drop_geometry() %>% 
-                                                  filter(!is.na(ltr_ID), scraped >= "2020-01-01") %>% 
-                                                  count(host_ID) %>% 
-                                                  filter(n > 1) %>% 
-                                                  arrange(desc(n)))[1,1]) %>% 
   nrow()
 
+(property %>%
+  st_drop_geometry() %>% 
+  filter(!is.na(ltr_ID)) %>% 
+  count(host_ID) %>% 
+  filter(n > 1) %>% 
+  arrange(desc(n)))[1,]
 
 #' [3] percentage of the hosts' properties which made the switch from STR to LTR
-property %>%
-  filter(!is.na(ltr_ID)) %>% 
+ltr_unique_property_ID %>%
   nrow() /
   property %>% 
   st_drop_geometry() %>% 
@@ -342,7 +378,20 @@ property %>%
          scraped >= "2020-01-01") %>% 
   nrow()
 
-#' [4] revenue distribution of hosts that matched on a LTR platform 
+#' The income distribution of the hosts that matched is greatly higher than the revenue 
+#' distribution of all Montreal hosts. Median revenue of all hosts was $4,300 in the 
+#' entire City of Montreal in 2019. For the hosts that matched, their yearly median 
+#' revenue was $14,300 [1]. Many of the top-earning hosts turned to LTR platforms in light 
+#' of the COVID-19 pandemic. For example, 27 [2] of the 37 hosts that made more than $500,000 
+#' in the past year moved at least one STR unit to the LTR market (28.9 [3] units in average 
+#' for these top earning hosts, versus 2.2 [3] units in average for the totality of hosts). 
+#' The hosts which moved one or more units from the STR market to the LTR have higher 
+#' revenue in general, and more than the majority of the big players of Montreal’s STR 
+#' market also shifted part of their units to the longer-term rental market. Needless 
+#' to say, they carry a very heavy weight in this analysis. Figure 5.4 displays the 
+#' revenue distribution of hosts that matched, compared to the ones that did not.
+
+#' [1] revenue distribution of hosts that matched on a LTR platform 
 revenue_2019 <- 
   daily %>%
   filter(housing,
@@ -357,7 +406,7 @@ revenue_2019 %>%
   filter(revenue_LTM > 0) %>%
   filter(host_ID %in% (property %>%
                          st_drop_geometry() %>%
-                         filter(property_ID %in% ltr_unique_property_ID$property_ID) %>%
+                         filter(!is.na(ltr_ID)) %>%
                          pull(host_ID))) %>%
   group_by(host_ID) %>% 
   summarize("host_rev" = sum(revenue_LTM)) %>% 
@@ -376,7 +425,7 @@ revenue_2019 %>%
   ) %>%
   opt_row_striping()
 
-#' [5] host that matched and made more than 500k
+#' [2] host that matched and made more than 500k
 half_mil_ltr <- revenue_2019 %>% 
   st_drop_geometry() %>%
   filter(revenue_LTM > 0) %>%
@@ -389,49 +438,70 @@ half_mil_ltr <- revenue_2019 %>%
   filter(host_rev > 500000) %>% 
   pull(host_ID) 
 
-#' [6] how many listings matched for the top earning hosts that matched
+half_mil_ltr %>% length()
+
+#' [3] how many listings matched for the top earning hosts that matched
 property %>% 
   st_drop_geometry() %>% 
   filter(host_ID %in% half_mil_ltr, 
-         !is.na(ltr_ID), 
-         scraped >= "2020-01-01") %>%
+         !is.na(ltr_ID)) %>%
   count(host_ID) %>% 
   summarize(mean(n))
 
 property %>% 
   st_drop_geometry() %>% 
-  filter(host_ID %in% (property %>%
-                         st_drop_geometry() %>% 
-                         filter(!is.na(ltr_ID)) %>% 
-                         pull(host_ID)), !is.na(ltr_ID), 
-         scraped >= "2020-01-01") %>%
+  filter(!is.na(ltr_ID)) %>%
   count(host_ID) %>% 
   summarize(mean(n))
 
-#' [7] Superhost status for host that matched
-property %>% # listings from hosts that matched
-  filter(host_ID %in% (property %>%
-                         st_drop_geometry() %>% 
-                         filter(!is.na(ltr_ID)) %>% 
-                         pull(host_ID)), scraped > "2020-01-01", superhost == T) %>% 
-  nrow() /
-  property %>% 
-  filter(host_ID %in% (property %>%
-                         st_drop_geometry() %>% 
-                         filter(!is.na(ltr_ID)) %>% 
-                         pull(host_ID)), scraped > "2020-01-01") %>% 
+
+#' Type of host: Superhost
+
+#' Another method of analyzing the type of host is the Superhost status. Hosts can earn S
+#' uperhost status through their performance on the site, such as their ratings and 
+#' reviews. Superhosts get more exposure in search results and are usually more 
+#' trusted by guests based on their standing. Out of all hosts that matched, 18.0% [1] had 
+#' Superhost status. This is significantly higher than the general distribution of hosts 
+#' in Montreal with superhost status (13.0% [2] of all hosts were Superhosts in 2020). 
+
+#' [1] Superhost status for host that matched
+property %>%
+  st_drop_geometry() %>% 
+  filter(!is.na(ltr_ID), superhost == T) %>% 
+  count(host_ID) %>% 
+  nrow() / 
+  property %>%
+  st_drop_geometry() %>% 
+  filter(!is.na(ltr_ID)) %>% 
+  count(host_ID) %>% 
   nrow()
 
-#' [8] Superhost status for ALL hosts 
-property %>% 
-  filter(scraped > "2020-01-01", superhost == T) %>% 
-  nrow() /
-  property %>% 
-  filter(scraped > "2020-01-01") %>% 
+#' [2] Superhost status for ALL hosts 
+property %>%
+  st_drop_geometry() %>% 
+  filter(scraped >= "2020-01-01", superhost == T) %>% 
+  count(host_ID) %>% 
+  nrow() / 
+  property %>%
+  st_drop_geometry() %>% 
+  filter(scraped >= "2020-01-01") %>% 
+  count(host_ID) %>% 
   nrow()
+  
+  
+  
 
+# Listing exposure ---------------------------------------------------
 
-# LTR listing exposure ---------------------------------------------------
+#' Length of availability on long-term rental platforms
+
+#' On average, STR matches found on LTR platforms stayed longer than the non-matches. 
+#' STR matches were listed on average 22.0 [1] days on LTR platforms, whereas the non-matches 
+#' stayed only 11.3 [1] days on average. The increased length of presence on LTR platforms 
+#' could be due to many factors, one of them being the higher median price (as mentioned 
+#' above) or less suitable features for the long-term rental market (i.e furnished units 
+#' or bedrooms set up like hotel rooms with multiple beds). Figure 5.5 shows the 
+#' distribution of the length of stay for both matches and non-matches.
 
 #' [1] length of availability on LTR platforms
 ltr_unique %>% 
@@ -440,6 +510,10 @@ ltr_unique %>%
   mutate(matched = if_else(!is.na(property_ID), TRUE, FALSE)) %>% 
   group_by(matched) %>% 
   summarize(mean(how_long_they_stay, na.rm = T))
+
+
+#' Remaining presence on STR platforms
+
 
 #' [2] number of matches that remained on STR platforms
 property %>%
