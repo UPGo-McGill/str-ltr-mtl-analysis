8721c741342fb4db7ff464159e6246b041de4904
Cloe St-Hilaire <cloe.st-hilaire@mail.mcgill.ca>
2020-09-07 21:38:47 -0400

    Updated table 3.2

--- R/32_chapter_3_analysis_fr.R
+++ R/32_chapter_3_analysis_fr.R
@@ -180,130 +180,201 @@ borough_housing_table %>%
   fmt_number(columns = 2:3, decimals = 0)
 
 
+# The impact of STRs on rental housing supply and vacancy rates -----------
+
+#' In 2019, the City of Montreal’s rental vacancy rate declined for the third 
+#' year in a row, reaching a 15-year low of 1.6% [1]. According to CMHC, a 
+#' healthy rental market should have a vacancy rate of at least 3%, but most 
+#' neighbourhoods in the central city are significantly below that. Le Sud-Ouest 
+#' had the city’s lowest vacancy rate, at 0.3% [2], which means that, of the 
+#' zone’s approximately 31,000 [3] rental apartments, fewer than 100 [4] were 
+#' available to be rented by prospective tenants in October 2019, when CMHC’s 
+#' survey was conducted. In general, vacancy rates are even lower for 
+#' family-sized housing units (defined by CMHC as units with two or more 
+#' bedrooms). For example, the vacancy rate for units with three or more 
+#' bedrooms was 0.5% [5] for Ville-Marie (the Downtown Montreal/Îles-des-Soeurs 
+#' zone), and 0.3% [6] in Le Plateau-Mont-Royal. 
+
+#' [1] 2019 city vacancy
+city_vacancy %>% 
+  filter(date == 2019, bedroom == "Total")
+
+#' [2] Le Sud-Ouest 2019 vacancy
+annual_vacancy %>% 
+  filter(zone == 2, date == 2019, dwelling_type == "Total", bedroom == "Total")
+
+#' [3] Total rental units in Le Sud-Ouest
+annual_units %>% 
+  filter(zone == 2, date == 2019, dwelling_type == "Total", 
+         bedroom == "Total") %>% 
+  pull(units) %>% 
+  round(-2)
+
+#' [4] Total vacant units in Le Sud-Ouest
+annual_units %>% 
+  left_join(annual_vacancy) %>% 
+  filter(zone == 2, date == 2019, dwelling_type == "Total", 
+         bedroom == "Total") %>% 
+  transmute(vacant_units = vacancy * units)
+
+#' [5] 3+ bedroom vacancy rate in VM
+annual_vacancy %>% 
+  filter(zone == 1, date == 2018, dwelling_type == "Total",
+         bedroom == "3 Bedroom +") %>% 
+  pull(vacancy)
+
+#' [6] 3+ bedroom vacancy rate in LPM
+annual_vacancy %>% 
+  filter(zone == 6, date == 2019, dwelling_type == "Total",
+         bedroom == "3 Bedroom +") %>% 
+  pull(vacancy)
+
+#' For example, in the Notre-Dame-de-Grâce/Côte-St-Luc zone, 66.9% [1] of 
+#' households are renters, so we assume that 66.9% of housing units converted to 
+#' dedicated STRs would have been rental housing, and the remaining 33.1% would 
+#' have been ownership housing.
+
+DA_probabilities_2019 %>% 
+  mutate(across(c(p_condo, p_renter), ~{.x * dwellings})) %>% 
+  mutate(across(where(is.numeric), ~if_else(is.na(.x), 0, as.numeric(.x)))) %>% 
+  select(dwellings, p_condo, p_renter, geometry) %>% 
+  st_interpolate_aw(cmhc, extensive = TRUE) %>% 
+  st_drop_geometry() %>% 
+  select(-Group.1) %>% 
+  rename(n_condo = p_condo, n_renter = p_renter) %>% 
+  cbind(cmhc, .) %>% 
+  as_tibble() %>% 
+  select(-geometry) %>% 
+  mutate(p_renter = n_renter / dwellings) %>% 
+  select(zone, p_renter) %>% 
+  filter(zone == 4)
+
+
 # The impact of STRs on residential rents ---------------------------------
 
+#' Between 2015 and 2019, we estimate that STRs have been responsible for a 
+#' 2.4% [1] increase in the average monthly rent. As average rents have risen 
+#' 16.9% [2] in this time period, this implies that one seventh of all rent 
+#' increases over the last five years have been caused by the growth of STRs. 
+#' Put differently, from 2015 to 2019, the average renter household in Montreal 
+#' has paid an additional $870 [3] in rent because of the impact of STRs on the 
+#' housing market.
 
+#' [1] Total rent increase
+rent_increase %>% 
+  slice(-1) %>% 
+  mutate(rent_increase = rent_increase + 1) %>% 
+  pull(rent_increase) %>% 
+  prod() %>% 
+  {. - 1} %>% 
+  round(3)
 
+#' [2] Average rent increase
+city_avg_rent %>% 
+  filter(bedroom == "Total") %>% 
+  filter(date >= 2014) %>% 
+  mutate(increase = slide_dbl(avg_rent, ~{.x[2] / .x[1]}, .before = 1)) %>% 
+  slice(-1) %>% 
+  pull(increase) %>% 
+  prod() %>% 
+  {. - 1} %>% 
+  round(3)
 
+#' [3] Extra rent payment
+city_avg_rent %>% 
+  filter(bedroom == "Total") %>% 
+  mutate(date = as.character(date)) %>% 
+  left_join(rent_increase, by = c("date" = "year_created")) %>% 
+  filter(date >= "2015") %>% 
+  select(date, avg_rent, rent_increase) %>% 
+  mutate(rent_plus_1 = 1 + rent_increase,
+         cumulative_increase = slide_dbl(rent_plus_1, prod, .before = 4),
+         extra_rent = (cumulative_increase - 1) * avg_rent * 12) %>% 
+  pull(extra_rent) %>% 
+  sum() %>% 
+  round(-1)
 
-
-
-
-
-
-
-
-# STR-induced housing loss - GH LISTINGS ----------------------------------------------------- 
-
-#' [1] average of active GH_units daily
-GH %>% 
+#' Table 3.2
+strs_by_zone <- 
+  property %>% 
+  st_intersection(cmhc) %>% 
   st_drop_geometry() %>% 
-  filter(date >= LTM_start_date, date <= LTM_end_date, status != "B") %>% 
-  group_by(date) %>% 
-  summarize(GH_units = sum(housing_units)) %>% 
-  summarize(round(mean(GH_units), digit=-1))
+  select(property_ID, zone) %>% 
+  left_join(daily, .) %>% 
+  filter(housing, date >= LTM_start_date, date <= LTM_end_date, 
+         status != "B") %>% 
+  count(zone) %>% 
+  mutate(active_strs = n / 365)
 
-#' [2] total ghost hostel listings
-GH_total <-
-  GH %>%
-  st_drop_geometry() %>%
-  filter(status != "B") %>% 
-  group_by(date) %>%
-  summarize(GH_units = sum(housing_units)) %>%
-  mutate(GH_average = frollmean(GH_units, 30, align = "right", fill = 130))
-
-
-# STR-induced housing loss - COMBINED HOUSING LOSS ----------------------------------------------------- 
-
-housing_loss <-
-  FREH %>%
-  select(date, FREH_3) %>% 
-  rename(`Entire home/apt` = FREH_3) %>%
-  left_join(GH_total, by = "date") %>%
-  select(-GH_units) %>% 
-  rename(`Private room` = GH_average) %>%
-  gather(`Entire home/apt`, `Private room`, 
-         key = `Listing type`, value = `Housing units`) 
-
-#' [1] housing loss in end 2019
-sum(filter(housing_loss, date == "2020-01-01")$`Housing units`) %>% 
-  round()
-
-#' [2] housing loss variation
-(housing_loss %>% 
-    filter(date == "2020-01-01") %>% 
-    summarize(sum(`Housing units`)) - 
-    housing_loss %>% 
-    filter(date == "2019-01-01") %>% 
-    summarize(sum(`Housing units`))
-) /
-  housing_loss %>% 
-  filter(date == "2019-01-01") %>% 
-  summarize(sum(`Housing units`))
-
-#' [3] Housing loss figure for a given year (the next day of the end of the year will give information on the previous year)
-sum(filter(housing_loss, date == "2019-01-01")$`Housing units`) %>% 
-  round(digits = -2)
-
-#' [4] housing loss of family size units in 2019
-(property %>% 
-    st_drop_geometry() %>% 
-    filter(bedrooms >= 2) %>% 
-    select(property_ID, bedrooms) %>% 
-    inner_join(daily) %>% 
-    filter(date == "2020-01-01") %>% 
-    summarize(sum(FREH_3)) + 
-    GH %>% 
-    st_drop_geometry() %>% 
-    filter(date >= LTM_start_date, date <= LTM_end_date, status != "B") %>% 
-    group_by(date) %>% 
-    summarize(GH_units = sum(housing_units)) %>% 
-    summarize(round(mean(GH_units)))) %>% 
-  round(digit =-2)
-
-#' [5] how many 2 bedrooms unit in housing loss
-property %>% 
-  st_drop_geometry() %>% 
-  filter(bedrooms == 2) %>% 
-  select(property_ID, bedrooms) %>% 
-  inner_join(daily) %>% 
-  filter(date == "2020-01-01") %>% 
-  summarize(sum(FREH_3)) %>% 
-  round(digits = -2)
+annual_avg_rent %>% 
+  filter(bedroom == "Total", occupied_status == "Occupied Units") %>% 
+  select(-bedroom, -occupied_status, -quality, -occ_rent_higher) %>% 
+  mutate(date = as.character(date)) %>% 
+  left_join(rent_increase_zone, by = c("zone", "date" = "year_created")) %>% 
+  group_by(zone, zone_name) %>%
+  mutate(rent_plus_1 = 1 + rent_increase,
+         cumulative_increase = slide_dbl(rent_plus_1, prod, .before = 4),
+         extra_rent = (cumulative_increase - 1) * avg_rent * 12) %>% 
+  summarize(avg_rent_2015 = avg_rent[date == 2015],
+            avg_rent_2019 = avg_rent[date == 2019],
+            rent_change = (avg_rent_2019 - avg_rent_2015) / avg_rent_2015,
+            str_rent_increase = prod(rent_plus_1) - 1,
+            str_share = str_rent_increase / rent_change,
+            extra_rent = sum(extra_rent)) %>% 
+  ungroup() %>% 
+  left_join(strs_by_zone) %>% 
+  relocate(active_strs, .after = zone_name) %>% 
+  select(-zone, -n) %>% 
+  add_row(
+    tibble_row(
+      zone_name = "Ville de Montréal",
+      active_strs = nrow(
+        filter(daily, housing, status != "B", date >= LTM_start_date, 
+               date <= LTM_end_date)) / 365,
+      avg_rent_2015 = (filter(city_avg_rent, date == 2015, 
+                              bedroom == "Total"))$avg_rent,
+      avg_rent_2019 = (filter(city_avg_rent, date == 2019, 
+                              bedroom == "Total"))$avg_rent,
+      rent_change = (avg_rent_2019 - avg_rent_2015) / avg_rent_2015,
+      str_rent_increase = (rent_increase %>% 
+                             slice(-1) %>% 
+                             mutate(rent_increase = rent_increase + 1) %>% 
+                             pull(rent_increase) %>% 
+                             prod()) - 1,
+      str_share = str_rent_increase / rent_change,
+      extra_rent = city_avg_rent %>% 
+        filter(bedroom == "Total") %>% 
+        mutate(date = as.character(date)) %>% 
+        left_join(rent_increase, by = c("date" = "year_created")) %>% 
+        filter(date >= "2015") %>% 
+        select(date, avg_rent, rent_increase) %>% 
+        mutate(rent_plus_1 = 1 + rent_increase,
+               cumulative_increase = slide_dbl(rent_plus_1, prod, .before = 4),
+               extra_rent = (cumulative_increase - 1) * avg_rent * 12) %>% 
+        pull(extra_rent) %>% 
+        sum())) %>% 
+  arrange(-active_strs) %>% 
+  slice(1:9) %>% 
+  mutate(across(c(active_strs, avg_rent_2015, avg_rent_2019, extra_rent), round, 
+                -1)) %>% 
+  set_names(c("Zone SCHL", "Annonces LCT actives quotidiennement (2019)", 
+              "Loyer moyen (2015)", "Loyer moyen (2019)",
+              "Augmentation totale des loyers (2015-2019)", 
+              "Augmentation du loyer induite par les LCT (2015-2019)",
+              "Part des LCT dans l'augmentation totale des loyers",
+              "Loyer supplémentaire moyen payé en raison des LCT (2015-2019)")) %>% 
+  gt() %>%
+  tab_header(title = "L'impact des LCT sur les loyers") %>%
+  opt_row_striping() %>% 
+  fmt_percent(columns = c(5:7), decimals = 1) %>% 
+  fmt_number(columns = 2:4, decimals = 0) %>% 
+  fmt_currency(8, decimals = 0)
 
 
-# STRs and Montreal’s housing market indicators - Vacancy rates ----------------------------------------------------- 
+# Nettoyage ----------------------------------------------------------------
 
-#
-
-
-
-
-
-
-
-
-
-
-
-#
-
-# STRs and Montreal’s housing market indicators - Average rent -----------------------------------------------------
-
-#
-
-
-
-
-
-
-
-
-
-
-
-
-
-#
-
-# STR-related increase in rent in Montreal, 2017-2019 -----------------------------------------------------
+rm(annual_avg_rent, annual_units, annual_vacancy, borough_housing_table,
+   boroughs, boroughs_raw, city, city_avg_rent, city_units, city_vacancy,
+   cmhc, DA, DA_probabilities_2017, DA_probabilities_2019, FREH, FREH_borough,
+   GH_borough, listing_probabilities_2017, listing_probabilities_2019, province,
+   rent_increase, rent_increase_zone, streets, streets_downtown, strs_by_zone)
