e11b3ef828c4ce3b9f99e3c7fe2f466340a367a2
David Wachsmuth <david.wachsmuth@mcgill.ca>
2020-09-08 15:19:08 -0400

    Script 60 finished?

--- R/60_matched_property_export.R
+++ R/60_matched_property_export.R
@@ -28,63 +28,73 @@ load("output/ltr_processed.Rdata")
 # Import landlord data ----------------------------------------------------
 
 kj_landlord <- 
-  readRDS("output/kj_with_landlord.rds") %>% 
+  readRDS("data/ltr/kj_with_landlord.rds") %>% 
   select(kj_id, Numero_de_matricule) %>%
   rename(id = kj_id, roll_number = Numero_de_matricule)
 
-# Clean ltr locations ---------------------------------------------------------------------
 
-street_no_pat <- START %R% one_or_more(DGT)
-postal_code_pat <- WRD %R% DGT %R% WRD %R% optional(SPC) %R% DGT %R% WRD %R% DGT
-street_pat <- START %R% one_or_more(WRD) %R% optional(SPC) %R% one_or_more(optional(WRD))
+# Clean LTR locations -----------------------------------------------------
 
-ltr <-
-ltr %>% 
+street_no_pat <- 
+  START %R% one_or_more(DGT)
+
+postal_code_pat <- 
+  WRD %R% DGT %R% WRD %R% optional(SPC) %R% DGT %R% WRD %R% DGT
+
+street_pat <- 
+  START %R% one_or_more(WRD) %R% optional(SPC) %R% one_or_more(optional(WRD))
+
+ltr <- 
+  ltr %>% 
   mutate(street_no = str_extract(location, pattern = street_no_pat),
-         location = str_remove(location, pattern = street_no_pat %R% optional(" ")),
-         
+         location = str_remove(location, 
+                               pattern = street_no_pat %R% optional(" ")),
          postal_code = str_extract(location, pattern = postal_code_pat),
          location = str_remove(location, pattern = postal_code_pat),
-         
          borough_loc = str_extract(location, pattern = boroughs$borough),
          location = str_remove(location, pattern = boroughs$borough),
-         
          location = str_remove_all(location, pattern = "Canada"),
-         location = str_remove_all(location, pattern = or("Montréal", "Montreal", "montreal", "montréal")),
+         location = str_remove_all(location, 
+                                   pattern = or("Montréal", "Montreal", 
+                                                "montreal", "montréal")),
          location = str_remove_all(location, pattern = or("QC", "Qc")),
-         
          street = str_extract(location, pattern = "[^,]+"),
          street = str_trim(street),
-         location = str_remove(location, pattern = "[^,]+" %R% optional(", ") %R% optional(", ") %R% optional(", ")),
-         
-         location = str_remove_all(location, pattern = ",")
-  ) %>% 
+         location = str_remove(location, 
+                               pattern = "[^,]+" %R% optional(", ") %R% 
+                                 optional(", ") %R% optional(", ")),
+         location = str_remove_all(location, pattern = ",")) %>% 
   rename(location_related = location) %>% 
-  mutate(location = if_else(!is.na(street_no), str_c(street_no, " ", street, ", ", postal_code),
-                                   postal_code),
-         location = if_else(is.na(postal_code), str_c(street_no, " ", street), location),
-         location = if_else(is.na(street_no) & is.na(postal_code), street, location))
+  mutate(location = if_else(!is.na(street_no), 
+                            str_c(street_no, " ", street, ", ", postal_code),
+                            postal_code),
+         location = if_else(is.na(postal_code), 
+                            str_c(street_no, " ", street), location),
+         location = if_else(is.na(street_no) & is.na(postal_code), street, 
+                            location))
 
-# Prepare the basic of the table ----------------------------------------------------------
+
+# Prepare table -----------------------------------------------------------
+
 listings_info <- 
   property %>% 
   st_drop_geometry() %>% 
   filter(!is.na(ltr_ID), scraped >= "2020-01-01") %>% 
-  select(property_ID, host_ID, created, scraped,
-         housing, listing_type, borough)
+  select(property_ID, host_ID, created, scraped, housing, listing_type, borough)
 
 
-# Add a location and roll_number column --------------------------------------------------
-ltr$location <-
-  str_replace_all(ltr$location, c("Ã©" = "é",
-                              "SÃ¨" = "è",
-                              "Ã¨" = "è"))
+# Add location and roll_number --------------------------------------------
+
+ltr <- 
+  ltr %>% 
+  mutate(location = str_replace_all(location, 
+                                    c("Ã©" = "é", "SÃ¨" = "è", "Ã¨" = "è")))
 
 locations_roll_number <- 
   ltr %>% 
   as_tibble() %>% 
   arrange(desc(scraped)) %>% 
-  distinct(id, .keep_all = T) %>% 
+  distinct(id, .keep_all = TRUE) %>% 
   filter(!is.na(property_ID)) %>% 
   unnest(property_ID) %>% 
   select(id, property_ID, location) %>% 
@@ -94,72 +104,77 @@ locations_roll_number <-
   filter(!is.na(location) | !is.na(roll_number)) %>% 
   rename(property_ID = property_ID) %>% 
   group_by(property_ID) %>% 
-  summarize(roll_number = list(roll_number),
-            location = list(location))
+  summarize(roll_number = list(roll_number), location = list(location))
+
+listings_info <- left_join(listings_info, locations_roll_number)
 
 
-listings_info <- 
-left_join(listings_info, locations_roll_number)
+# Add FREH and GH ---------------------------------------------------------
 
-
-# Adding a FREH column -----------------------------------------------------------------
-# If listing has been following a FREH pattern at least 3 months once in its lifetime
 FREH <- 
   daily %>% 
-  filter(FREH_3 >= 0.5) %>% 
+  filter(FREH_3 > 0.5) %>% 
   select(property_ID) %>% 
   distinct() %>% 
-  mutate(FREH = T)
+  mutate(FREH = TRUE)
 
 listings_info <- 
   left_join(listings_info, FREH) %>% 
-  mutate(FREH = if_else(is.na(FREH), F, T))
+  mutate(FREH = if_else(is.na(FREH), FALSE, TRUE))
 
-# Adding a GH column -------------------------------------------------------------------
-# If listing has been part of a GH once in its lifetime
 GH_properties <- 
   GH %>% 
   st_drop_geometry() %>% 
-  distinct(ghost_ID, .keep_all = T) %>% 
+  distinct(ghost_ID, .keep_all = TRUE) %>% 
   unnest(property_IDs) %>% 
   distinct(property_IDs) %>% 
   rename(property_ID = property_IDs) %>% 
-  mutate(GH = T)
+  mutate(GH = TRUE)
 
 listings_info <- 
   left_join(listings_info, GH_properties) %>% 
-  mutate(GH = if_else(is.na(GH), F, T))
+  mutate(GH = if_else(is.na(GH), FALSE, TRUE))
 
-# Still on STR platform ----------------------------------------------------------------
+
+# STR still listed? -------------------------------------------------------
+
 listings_info <- 
-listings_info %>% 
-  mutate(still_listed = if_else(months(scraped) == months(max(scraped)), T, F)) %>% 
+  listings_info %>% 
+  mutate(still_listed = if_else(scraped >= "2020-07-31", TRUE, FALSE)) %>% 
   select(-scraped)
 
-# Still active -------------------------------------------------------------------------
+
+# STR still active? -------------------------------------------------------
+
 still_active_properties <- 
-daily %>% 
-  filter(date >= "2020-05-01", date < "2020-06-01", status != "B") %>% 
+  daily %>% 
+  filter(date >= "2020-07-01", status != "B") %>% 
   distinct(property_ID) %>% 
-  mutate(still_active = T)
+  mutate(still_active = TRUE)
 
 listings_info <- 
-left_join(listings_info, still_active_properties) %>% 
-  mutate(still_active = if_else(is.na(still_active), F, T))
+  listings_info %>% 
+  left_join(still_active_properties) %>% 
+  mutate(still_active = if_else(is.na(still_active), FALSE, TRUE))
+
   
-# How many other STR are operated by this host -----------------------------------------
+# How many other listings operated by the host? ---------------------------
+
 host_properties <- 
-property %>% 
+  property %>% 
   st_drop_geometry() %>% 
   filter(scraped >= "2020-01-01") %>% 
   count(host_ID) 
 
 listings_info <- 
-left_join(listings_info, host_properties) %>% 
+  listings_info %>% 
+  left_join(host_properties) %>% 
   mutate(n = n - 1) %>% # for it to be all "other" listings of a host
   rename(host_other_listings = n)
 
-# Addition of LTR IDs ------------------------------------------------------------------
+
+# LTR IDs -----------------------------------------------------------------
+
 ltr_id <- 
   ltr %>% 
   st_drop_geometry %>% 
@@ -172,19 +187,25 @@ ltr_id <-
   summarize(ltr_ID = list(id))
 
 listings_info <- 
-  left_join(listings_info, ltr_id)
+  listings_info %>% 
+  left_join(ltr_id)
 
-  
 
-# ordering columns ---------------------------------------------------------------------
+# Airbnb URL --------------------------------------------------------------
+
+listings_info <- 
+  listings_info %>% 
+  mutate(url = paste0("https://airbnb.ca/rooms/", str_remove(property_ID, 
+                                                             "ab-")))
+
+
+# Order and rename columns ------------------------------------------------
+
 listings_info <- 
   listings_info %>% 
   select(property_ID, ltr_ID, housing, listing_type, host_ID, 
          host_other_listings, created, still_listed, still_active, borough, 
-         location, roll_number, FREH, GH)
-
-
-# save the file ------------------------------------------------------------------------
+         location, roll_number, FREH, GH, url)
 
 listings_info <- 
   listings_info %>%
@@ -207,8 +228,7 @@ listings_info <-
          `Adresse postale` = location,
          `Numéro de matricule` = roll_number,
          `Logement entier fréquemment loué` = FREH,
-         `Auberge fantôme` = GH)
+         `Auberge fantôme` = GH,
+         URL = url)
 
 write.csv(listings_info, "output/listings_info.csv")
-
-listings_info %>% View()
